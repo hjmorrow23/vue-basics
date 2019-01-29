b8513721a085e44a710cc9bd606af648e6c419be
Henry Morrow <henry@workwithgrid.com>
2019-01-28 19:12:46 -0500

    Lists and edit

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,19 +1,40 @@
 <template>
     <section class="content-wrapper">
-        <h1 class="user-name">{{users[currentUserId].name}}</h1>
-        <p class="user-description">{{users[currentUserId].description}}</p>
-        <article class="likes user-details">
-            <h2>Likes</h2>
-            <ul>
-                <li v-for="like in users[currentUserId].likes">{{like}}</li>
-            </ul>
-        </article>
-        <article class="dislikes user-details">
-            <h2>Dislikes</h2>
-            <ul>
-                <li v-for="dislike in users[currentUserId].dislikes">{{dislike}}</li>
-            </ul>
-        </article> 
+        <div v-if="!editing">
+            <h1 class="user-name">{{users[currentUserId].name}}</h1>
+            <p class="user-description">{{users[currentUserId].description}}</p>
+            <article class="likes user-details">
+                <h2>Likes</h2>
+                <ul>
+                    <li v-for="like in users[currentUserId].likes">{{like}}</li>
+                </ul>
+            </article>
+            <article class="dislikes user-details">
+                <h2>Dislikes</h2>
+                <ul>
+                    <li v-for="dislike in users[currentUserId].dislikes">{{dislike}}</li>
+                </ul>
+            </article> 
+        </div>
+        <div v-else>
+            <label>Name:</label>
+            <input type="text" class="user-input" v-model="users[currentUserId].name" />
+            <label>Description:</label>
+            <input type="text" class="user-input" />
+            <!-- <article class="likes user-details">
+                <h2>Likes</h2>
+                <ul>
+                    <li v-for="like in users[currentUserId].likes">{{like}}</li>
+                </ul>
+            </article>
+            <article class="dislikes user-details">
+                <h2>Dislikes</h2>
+                <ul>
+                    <li v-for="dislike in users[currentUserId].dislikes">{{dislike}}</li>
+                </ul>
+            </article>  -->
+        </div>
+        <a href="" class="edit-button" @click.prevent="toggleEdit">Edit</a>
     </section>
 </template>
 
@@ -23,6 +44,16 @@
         props: {
             users: Array,
             currentUserId: Number
+        },
+        data() {
+            return {
+                editing: false
+            }
+        },
+        methods: {
+            toggleEdit: function() {
+                this.editing = !this.editing;
+            }
         }
     }
 </script>
@@ -37,13 +68,21 @@
 
     .user-description {
         width: 50%;
-        margin: 0 auto;
+        margin: 0 auto 50px;
     }
 
     .user-details {
         width: 50%;
         display: inline-block;
         vertical-align: top;
+    }
+
+    .user-input {
+        width: 200px;
+        border: none;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+        display: block;
+        margin: 0 auto 50px;
     }
 
     ul {
